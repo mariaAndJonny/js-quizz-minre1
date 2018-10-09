232ce78c6f0e913e68fcbd4c1b93f188e6272d45
minre1 <43936502+minre1@users.noreply.github.com>
2018-10-09 10:09:18 +0200

    Update math.js
    homework done

--- math.js
+++ math.js
@@ -1,15 +1,29 @@
 exports.add = function (x, y) {
-    // ToDo: Add the necessary code to add two numbers
-    return NaN;
+
+  return x + y;
+
 }
+
+
 
 exports.sub = function (x, y) {
-    // ToDo: Add the necessary code to subtract two numbers
-    return NaN;
+
+  return x - y;
+
 }
 
+
+
 exports.sumOfPositiveNumbers = function (upperLimitInclusive) {
-    // ToDo: Add the necessary code to calculate the sum of all positive
-    //       numbers lower equal the specified upper limit.
-    return NaN;
+
+  let result = 0;
+
+  for (let i = 0; i <= upperLimitInclusive; i++) {
+
+    result += i;
+
+  }
+
+  return result;
+
 }
